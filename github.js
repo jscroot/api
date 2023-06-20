f1959a512a7230e794ffa80f5fb27d93b8c01d52
Rolly Maulana Awangga <awangga@gmail.com>
2023-06-20 14:35:59 +0700

    create github api

---
+++ github.js
@@ -0,0 +1,52 @@
+export function postWithToken(target_url,tokenkey,tokenvalue,datajson,responseFunction){
+    var myHeaders = new Headers();
+    myHeaders.append(tokenkey, tokenvalue);
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify(datajson);
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+    .then(response => response.text())
+    .then(result => responseFunction(JSON.parse(result)))
+    .catch(error => console.log('error', error));
+}
+
+export function postWithBearer(target_url,token,datajson,responseFunction){
+    var myHeaders = new Headers();
+    myHeaders.append("Accept", "application/vnd.github+json");
+    myHeaders.append("Authorization", "Bearer "+token);
+    myHeaders.append("X-GitHub-Api-Version", "2022-11-28");
+
+    var raw = JSON.stringify(datajson);
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+    .then(response => response.text())
+    .then(result => responseFunction(JSON.parse(result)))
+    .catch(error => console.log('error', error));
+}
+
+export function get(target_url,responseFunction){
+    var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+    .then(response => response.text())
+    .then(result => responseFunction(JSON.parse(result)))
+    .catch(error => console.log('error', error));
+}
